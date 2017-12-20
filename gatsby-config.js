8f7c57fb36bcc46bfa2b342b62f8a6fdbf2a8f27
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-20 15:10:57 +0000

    [init]

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
-const config = require('./data/site-config.js');
+const config = require('./site-config/');
 
 module.exports = {
     siteMetadata: {
-        title: 'Arden de Raaij - Front-end Web Developer',
+        title: 'Gatsby ',
         siteUrl: config.siteUrl,
     },
     plugins: [
@@ -13,40 +13,8 @@ module.exports = {
         {
             resolve: 'gatsby-source-filesystem',
             options: {
-                name: 'posts',
-                path: `${__dirname}/content/posts`,
-            },
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
                 name: 'pages',
                 path: `${__dirname}/src/pages`,
-            },
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: `${__dirname}/src/assets/img`,
-            },
-        },
-        {
-            resolve: 'gatsby-transformer-remark',
-            options: {
-                plugins: [
-                    'gatsby-remark-prismjs',
-                    'gatsby-remark-smartypants',
-                    'gatsby-remark-copy-linked-files',
-                    'gatsby-remark-wrap-iframe',
-                    {
-                        resolve: 'gatsby-remark-images',
-                        options: {
-                            maxWidth: 1600,
-                            linkImagesToOriginal: false,
-                        },
-                    },
-                ],
             },
         },
         {
