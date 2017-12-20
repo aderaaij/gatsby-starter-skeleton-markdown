6d5d6d9856d136c8860c4c576f5749463ea4416f
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-20 16:35:40 +0000

    [add] more sitconfig options

--- site-config/index.js
+++ site-config/index.js
@@ -1,14 +1,16 @@
 module.exports = {
     siteName: 'Gatsby Starter Skeleton',
     siteLogo: '/logo/logo_space.png',
-    siteUrl: 'https://www.arden.nl',
+    siteUrl: 'https://www.yoursite.com', // No trailing slash!
     siteTitle: 'Gatsby Starter Skeleton',
-    siteDescription: 'A starter skeleton for Gatsby', // Website description used for RSS feeds/meta description tag.
+    siteDescription: 'A starter skeleton for Gatsby',
     siteLanguage: 'en_US',
+    siteColor: '#ffffff',
     typekitID: '',
+    faviconDir: '/favicons/', // If you want to add favicons, generate them at https://realfavicongenerator.net/
     contact: {
         twitter: '',
         facebook: '',
-        email: '',
+        email: 'test@test.test',
     },
 };
