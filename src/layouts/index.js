79560631e4d4c658bcf7cb5004701998bbd9cc38
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-20 20:23:26 +0000

    [add] proptype validation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import Helmet from 'react-helmet';
-import { injectGlobal } from 'emotion';
+import PropTypes from 'prop-types';
 import 'normalize.css';
-import config from '../../site-config/';
 import Head from '../components/Head/Head';
-import Typekit from '../components/Head/Typekit';
-import Piwik from '../components/Head/Piwik';
-
-injectGlobal`
-    * {
-        box-sizing: border-box;
-    }
-`;
 
 const TemplateWrapper = ({ children }) => (
     <div>
         <Head />
-        {config.typekitID &&
-            <Typekit id={config.typekitID} />
-        }
         {children()}
     </div>
 );
 
 export default TemplateWrapper;
+
+TemplateWrapper.propTypes = {
+    children: PropTypes.func.isRequired,
+};
