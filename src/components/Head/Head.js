3fa638f980f7da4bf48be5f6e3b8e714b6a0aaee
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-20 20:23:06 +0000

    [update] head component

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { injectGlobal } from 'emotion';
 import config from '../../../site-config/';
-import Piwik from './Piwik';
 import Typekit from './Typekit';
 
 const Head = props => (
-    <Helmet
-        title={config.siteDescription}
-        htmlAttributes={{ lang: 'EN' }}
-    >
-        {/* <Piwik /> */}
-
-        {/* <Typekit id={config.typekitID} /> */}
-
-    </Helmet>
+    <div>
+        <Helmet
+            title={config.siteDescription}
+            htmlAttributes={{ lang: 'EN' }}
+        />
+        {config.typekitID &&
+            <Typekit id={config.typekitID} />
+        }
+    </div>
 );
 
 export default Head;
