1e44db7fdb8947aeac7ba7004f17c48151e92faa
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-20 16:35:57 +0000

    [add] a single head property for the layout page

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { injectGlobal } from 'emotion';
 import config from '../../../site-config/';
-import Piwik from '../Piwik/Piwik';
-import Typekit from '../Typekit/Typekit';
+import Piwik from './Piwik';
+import Typekit from './Typekit';
 
 const Head = props => (
-    <div>
-        <Helmet
-            title={config.siteDescription}
-            htmlAttributes={{ lang: 'EN' }}
-        />
-        <Piwik />
-        <Typekit />
-        {children()}
-    </div>
+    <Helmet
+        title={config.siteDescription}
+        htmlAttributes={{ lang: 'EN' }}
+    >
+        {/* <Piwik /> */}
+
+        {/* <Typekit id={config.typekitID} /> */}
+
+    </Helmet>
 );
 
 export default Head;
