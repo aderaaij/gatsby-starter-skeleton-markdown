74cc617e24c53697895cb8aaa1c80623c450d6f2
Arden de Raaij <a.de.raaij@gmail.com>
2017-12-20 16:38:04 +0000

    [cleanup]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
 import SEO from '../components/SEO/SEO';
+import config from '../../site-config/';
 
-
-const Home = props => (
+const Home = () => (
     <div>
-        <h1>sup?</h1>
+        <SEO />
+        <h1>{config.siteTitle}</h1>
+        <p>{config.siteDescription}</p>
     </div>
 );
 
