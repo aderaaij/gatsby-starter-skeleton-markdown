ced349242c2757807a0c3fa678bcd5fc4d2ed7bf
Arden de Raaij <a.de.raaij@gmail.com>
2018-01-02 22:14:51 +0000

    [add] header and nav

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
 import 'normalize.css';
+import config from '../../site-config/';
 import Head from '../components/Head/Head';
 
 const TemplateWrapper = ({ children }) => (
-    <div>
+    <div style={{ padding: 20 }}>
         <Head />
+        <h1><Link to="/">{config.meta.siteTitle}</Link></h1>
+        <nav>
+            <ul>
+                <li><Link to="posts">Posts</Link></li>
+                <li />
+                <li />
+            </ul>
+        </nav>
         {children()}
     </div>
 );
