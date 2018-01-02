794756a0b8bccdff8361405ac0aca9b58b11af02
Arden de Raaij <a.de.raaij@gmail.com>
2018-01-02 22:40:24 +0000

    [add] key to tag, restructure

--- src/components/PostExcerpt/PostExcerpt.js
+++ src/components/PostExcerpt/PostExcerpt.js
@@ -13,18 +13,24 @@ const PostExcerpt = (props) => {
             {!published &&
             <span>unpublished</span>
             }
-            <span>category: <Link to={`/categories/${category}`}>{category}</Link></span>
             <h2>
                 <Link to={path}>{title}</Link>
             </h2>
             <p>{excerpt}</p>
             {(tags || date) &&
             <div>
-                <div>tags: {tags.map(tag => (
-                    <Link to={`/tags/${tag}`}>
-                        <span>{tag}</span>
-                    </Link>
-                ))}
+                <span>category: <Link to={`/categories/${category}`}>{category}</Link></span>
+                <div>
+                    tags:
+                    <ul>{tags.map((tag, i) => (
+                        <li key={tag}>
+                            <Link to={`/tags/${tag}`}>
+
+                                <span>{tag}</span>
+                            </Link>
+                        </li>
+                    ))}
+                    </ul>
                 </div>
                 <span>date: {date}</span>
             </div>
