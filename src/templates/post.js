79c4e499aea61e48e8a305203d665a780dae6241
Arden de Raaij <a.de.raaij@gmail.com>
2018-01-04 13:47:59 +0000

    [refactor] into stateless component

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,41 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import Img from 'gatsby-image';
 import SEO from '../components/SEO/SEO';
 import '../graphql/post';
 
-class BlogPost extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fadeIn: false,
-        };
-    }
-
-    componentDidMount() {
-        this.setState({
-            fadeIn: true,
-        });
-    }
-
-    render() {
-        const { markdownRemark } = this.props.data;
-        const { frontmatter, html } = markdownRemark;
-        const { published } = frontmatter;
-        return (
-            <article>
-                <SEO type="post" post={markdownRemark} />
-                {!published &&
-                <Helmet>
-                    <meta name="robots" content="noindex" />
-                </Helmet>
-                }
-                <h1>{frontmatter.title}</h1>
-                <div dangerouslySetInnerHTML={{ __html: html }} />
-            </article>
-        );
-    }
-}
+const BlogPost = (props) => {
+    const { markdownRemark } = props.data;
+    const { frontmatter, html } = markdownRemark;
+    const { published } = frontmatter;
+    return (
+        <article>
+            <SEO type="post" post={markdownRemark} />
+            {!published &&
+            <Helmet>
+                <meta name="robots" content="noindex" />
+            </Helmet>
+            }
+            <h1>{frontmatter.title}</h1>
+            <Img sizes={frontmatter.cover.childImageSharp.sizes} />
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+        </article>
+    );
+};
 
 BlogPost.propTypes = {
     data: PropTypes.object.isRequired,
