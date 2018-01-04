3c7fbf269bb15917aad107e3853aa785c7d13106
Arden de Raaij <a.de.raaij@gmail.com>
2018-01-04 14:20:44 +0000

    [create] tag template

---
+++ src/templates/tag.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
+import SEO from '../components/SEO/SEO';
+import PostsList from '../components/PostsList/PostsList';
+import config from '../../site-config';
+import '../graphql/archive';
+
+const TagTemplate = (props) => {
+    const { edges } = props.data.allMarkdownRemark;
+    const { category } = props.pathContext;
+    return (
+        <div>
+            <SEO />
+            <Helmet>
+                <title>{`Posts tagged with '${category}'  | ${config.meta.siteName}`}</title>
+                <link rel="canonical" href={`${config.siteUrl}/about/`} />
+            </Helmet>
+            <PostsList edges={edges} />
+        </div>
+    );
+};
+
+TagTemplate.propTypes = {
+    data: PropTypes.object.isRequired,
+    pathContext: PropTypes.object.isRequired,
+};
+
+export default TagTemplate;
+
+export const query = graphql`
+    query TagArchive($tag: String) {
+        allMarkdownRemark(
+            limit: 1000
+            sort: { fields: [frontmatter___date], order: DESC }
+            filter: { frontmatter: { tags: { in: [$tag] } } }
+        ) {
+            totalCount
+            edges {
+                node {
+                    ...defaultArchiveQuery
+                }
+            }
+        }
+    }
+`;
